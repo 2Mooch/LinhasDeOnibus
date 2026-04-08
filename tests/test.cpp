be9caee7254b307e9e09571c918a503e7d5eb6ea
Victor Erthal <erthalv22@gmail.com>
2026-04-07 22:14:18 -0300

    Update test.cpp to include RotaOnibus functionality

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,16 @@
+#include "../include/RotaOnibus.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Teste funcionando." << std::endl;
+    RotaOnibus r;
+
+    r.inserirNoFim("Centro");
+    r.inserirNoFim("Shopping");
+    r.inserirNoInicio("Garagem");
+
+    r.listarRotaIda();
+
+    std::cout << "Total: " << r.contarParadas() << std::endl;
+
     return 0;
 }
